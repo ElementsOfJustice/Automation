6216d01b2be793c77c596ed96e05877eb217b788
123connorpoop <rct3fan55@yahoo.com>
2022-06-19 21:28:44 -0400

    Finish parse_textGrid and add some examples

--- VoiceLineProcessor/parse_textGrid.py
+++ VoiceLineProcessor/parse_textGrid.py
@@ -1,7 +1,36 @@
 import textgrid
+import os
+
+
+def writeParsedFile(source, destination):
+    tg = textgrid.TextGrid.fromFile(source)
+    words = "var words = [ \n"
+    phonemes = "var phonemes = [ \n"
+    for j in tg[0]:
+        words += "[" + str(j.minTime) + "," + str(j.maxTime) + \
+            ",\"" + str(j.mark) + "\"], \n"
+    words += "];"
+    for j in tg[1]:
+        phonemes += "[" + str(j.minTime) + "," + \
+            str(j.maxTime) + ",\"" + str(j.mark) + "\"], \n"
+    phonemes += "];"
+    f = open(destination, "x")
+    f.write(words + "\n" + phonemes)
+    f.close()
+
 
 def main():
-   tg = textgrid.TextGrid.fromFile('C:/Users/rct3f/AppData/Local/Adobe/Animate 2022/en_US/Configuration/Commands/VoiceLineProcessor/s5_151_fair_devotion.TextGrid')
-   print(tg[1][1])
+    directory_name = "TextGrid_files"
+    directory = os.fsencode(directory_name)
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        try:
+            writeParsedFile(directory_name + "/" + filename,
+                           "output/" + filename.rsplit(".", 1 )[0] + ".cfg")
+        except:
+            print("fail")
+            exit()
+
+
 main()
-    
