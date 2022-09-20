812ae72a7ea22a150bdfdbd3a3ad89d40947b008
123connorpoop <rct3fan55@yahoo.com>
2022-09-19 22:16:35 -0400

    Reorder some commands, Utils, lipsyncing Template

--- C Level Utilities/Utils.c
+++ C Level Utilities/Utils.c
@@ -6,17 +6,18 @@
 
 // A sample function
 // Every implementation of a Javascript function must have this signature
-JSBool getFLACLength(JSContext *cx, JSObject *obj,  unsigned int argc,   jsval *argv, jsval *rval)
+JSBool getFLACLength(JSContext* cx, JSObject* obj, unsigned int argc, jsval* argv, jsval* rval)
 {
     unsigned int wasteOfMemory = 0; // I suck with C but this works to create a string ;-;
     unsigned short* x = JS_ValueToString(cx, argv[0], &wasteOfMemory);
-    char* filename = (char *) malloc(wasteOfMemory); // allocate memory for filename
+    char* filename = (char*)malloc(wasteOfMemory + 1); // allocate memory for filename
     FILE* fp; // prepare file to read from
     int c, i, max; // looping variables
     unsigned long sampleRate = 0, samples = 0; // file metadata variables
     for (int i = 0; i < wasteOfMemory; i++) {
         filename[i] = (char)x[i]; // get filename 
     }
+    filename[wasteOfMemory] = '\0'; // null terminator at the end of string
     int err = fopen_s(&fp, filename, "rb"); // open the file
     if (err != 0) {
         *rval = JS_IntegerToValue(-1);
@@ -25,7 +26,7 @@ JSBool getFLACLength(JSContext *cx, JSObject *obj,  unsigned int argc,   jsval *
     for (i = 0, max = 0x20; i < max && (c = getc(fp)) != EOF; i++) { // loop over first 32 bytes of the file
         if ((i >= 0x12 && i <= 0x14) || (i >= 0x15 && i <= 0x19))
             if (i >= 0x12 && i <= 0x14) { // FLAC documentation says that the sample rate is from byte 0x12 to the first nibble of byte 0x14
-                sampleRate <<= (i == 0x14) ? 4 : 8; 
+                sampleRate <<= (i == 0x14) ? 4 : 8;
                 sampleRate += (i == 0x14) ? c & 0b11110000 : c;
             }
         if (i >= 0x15 && i <= 0x19) { // FLAC documentation says that the number of samples is from the second nibble of byte 0x15 to the end of byte 0x19
@@ -36,11 +37,11 @@ JSBool getFLACLength(JSContext *cx, JSObject *obj,  unsigned int argc,   jsval *
     }
     int time = 1000 * (1.0f * samples / (1.0f * sampleRate)); // the duration of a sound file is the number of samples divided by the sample rate. Truncate to the nearest millisecond
     fclose(fp);
-	
-	*rval = JS_IntegerToValue(time); // return the duration of the FLAC file in milliseconds
+
+    *rval = JS_IntegerToValue(time); // return the duration of the FLAC file in milliseconds
     free(filename); // free memory alloated for the string
-	// Indicate success
-	return JS_TRUE;
+    // Indicate success
+    return JS_TRUE;
 }
 
 
@@ -54,7 +55,6 @@ MM_STATE
 void
 MM_Init()
 {
-	// sample function
-	JS_DefineFunction(_T("getFLACLength"),			getFLACLength,			1);
+    // sample function
+    JS_DefineFunction(_T("getFLACLength"), getFLACLength, 1);
 }
-	
