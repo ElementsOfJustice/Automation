01ab47510423091dee229a2f7e1dba8b533d433a
The_Soundman <103096955+ExodexoDev@users.noreply.github.com>
2023-04-22 15:00:33 -0500

    Added Joke Getter to Sound
    We will move it out of Sound eventually!

--- C++ Level Utilities/Sound.cpp
+++ C++ Level Utilities/Sound.cpp
@@ -6,8 +6,11 @@
 #include <sstream>
 #include <vector>
 #include <thread>
+#include <winhttp.h>
+#include <iostream>
 
 #pragma comment(lib, "winmm.lib")  // Link with winmm.lib to use the Windows multimedia functions
+#pragma comment(lib, "winhttp.lib")
 
 std::vector<std::string> splitOnQuestionMark(std::string input) {
     std::vector<std::string> substrings;
@@ -81,6 +84,71 @@ JSBool playSound(JSContext* cx, JSObject* obj, unsigned int argc, jsval* argv, j
     return JS_TRUE;
 }
 
+JSBool joke(JSContext* cx, JSObject* obj, unsigned int argc, jsval* argv, jsval* rval) {
+    
+    // Initialize WinHTTP.
+    HINTERNET hSession = WinHttpOpen(L"WinHTTP Example/1.0",
+        WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME,
+        WINHTTP_NO_PROXY_BYPASS, 0);
+    if (!hSession) {
+        std::cerr << "WinHttpOpen failed: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    // Create an HTTP request.
+    HINTERNET hRequest = WinHttpOpenRequest(WinHttpConnect(hSession,
+        L"v2.jokeapi.dev", INTERNET_DEFAULT_HTTPS_PORT, 0),
+        L"GET", L"/joke/Programming,Miscellaneous,Pun,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist", NULL, WINHTTP_NO_REFERER,
+        WINHTTP_DEFAULT_ACCEPT_TYPES, WINHTTP_FLAG_SECURE);
+    if (!hRequest) {
+        std::cerr << "WinHttpOpenRequest failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    // Send the HTTP request and receive the response.
+    if (!WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+        WINHTTP_NO_REQUEST_DATA, 0, 0, 0) || !WinHttpReceiveResponse(hRequest, NULL)) {
+        std::cerr << "WinHttpSendRequest/WinHttpReceiveResponse failed: "
+            << GetLastError() << std::endl;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    // Read the HTTP response body.
+    DWORD dwSize = 0;
+    if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) {
+        std::cerr << "WinHttpQueryDataAvailable failed: " << GetLastError() << std::endl;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    //Get a warning here LOL
+    char* pszOutBuffer = new char[dwSize + 1];
+    ZeroMemory(pszOutBuffer, dwSize + 1);
+    DWORD dwDownloaded = 0;
+    if (!WinHttpReadData(hRequest, pszOutBuffer, dwSize, &dwDownloaded)) {
+        std::cerr << "WinHttpReadData failed: " << GetLastError() << std::endl;
+        delete[] pszOutBuffer;
+        WinHttpCloseHandle(hRequest);
+        WinHttpCloseHandle(hSession);
+        return 1;
+    }
+
+    // Print the joke.
+    std::string str(pszOutBuffer);
+    JS_StringToValue(cx, stringToWide(str), str.size(), rval);
+
+    // Clean up.
+    delete[] pszOutBuffer;
+    WinHttpCloseHandle(hRequest);
+    WinHttpCloseHandle(hSession);
+
+    return JS_TRUE;
+}
+
 extern "C" {
     // MM_STATE is a macro that expands to some definitions that are
     // needed in order interact with Dreamweaver.  This macro must be
@@ -93,5 +161,6 @@ extern "C" {
     {
         JS_DefineFunction(L"beep", beep, 512);
         JS_DefineFunction(L"playSound", playSound, 512);
+        JS_DefineFunction(L"joke", joke, 512);
     }
 }
